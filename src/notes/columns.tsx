23597ab0a46c19e348b2ef44aba4d451ced6ddc5
padmanabhan08 <padmanabhan123345@gmail.com>
2026-01-13 17:03:42 +0530

    sort fix

--- src/notes/columns.tsx
+++ src/notes/columns.tsx
@@ -21,21 +21,7 @@ type Patient = {
 
 
 
-export const columns: ColumnDef<Patient>[] = [{
-  accessorKey:"id",
-  header: ({ column }) => {
-
-    return (
-        <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Id
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-    )}
-},
-  
+export const columns: ColumnDef<Patient>[] = [
     {
 
     accessorKey: "patient",
